600440efda0190160a0111505402187c88297efd
Seng Ngo <sengngo@gmail.com>
2015-04-18 21:09:00 +0000

    Lesson  13

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 
+gem 'simple_form'
+
 #gem 'will_paginate', '~> 3.0.7'
 gem 'will_paginate-bootstrap'
 
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,14 @@
 class PlacesController < ApplicationController
 	def index
- 		 @places = Place.all
+ 		 @place = Place.all
  		 @place = Place.paginate :per_page => 5, :page => params[:page],
 								:conditions => ['name like ?', "%#{params[:search]}%"],
 								:order => 'name'
 	end
+
+	def new
+		@place = Place.new
+	end
+
 
 end
