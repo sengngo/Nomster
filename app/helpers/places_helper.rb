9678883eb1ea0d3d77ba9f7e45c5a9659524e42c
Seng Ngo <sengngo@gmail.com>
2015-04-21 00:47:13 +0000

    deleted places_helper

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,14 +1,2 @@
 module PlacesHelper
-	def comment_drop
-		comment.place.try(:name)
-	end
 end
-
-def create
-		@place = current_user.places.create(place_params)
-		if @place.valid?
-			redirect_to root_path
-		else
-			render :new, :status => :unprocessable_entity
-		end
-	end
